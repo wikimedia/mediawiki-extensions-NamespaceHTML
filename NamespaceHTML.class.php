1761a689b7598716b3b98da9950d93652572916b
Ike Hecht <tosfos@yahoo.com>
2014-06-11 14:14:22 -0700

    fix for $parser->getTitle() returning null
    Change-Id: Id32b7a20ad669a1bd8825796d30f765253d47dcf

--- NamespaceHTML.class.php
+++ NamespaceHTML.class.php
@@ -14,34 +14,52 @@ class NamespaceHTML {
 	 * @return boolean
 	 */
 	static function addNamespaceHTML( Parser &$parser ) {
-		global $wgRawHtml, $wgRawHtmlNamespaces;
+		global $wgRawHtml;
 
 		# If raw HTML allowed wiki-wide, don't do anything.
 		if ( $wgRawHtml ) {
 			return true;
 		}
 
+		$parser->setHook( 'html', array( __CLASS__, 'html' ) );
+
+		return true;
+	}
+
+	/**
+	 * Pass everything to core parser tag hook function for 'html'.
+	 * Enabled even when $wgRawHtml is disabled.
+	 *
+	 * This is potentially unsafe and should be used only in protected
+	 * namespaces, as the contents are emitted as raw HTML.
+	 *
+	 * Uses undocumented extended tag hook return values, introduced in r61913.
+	 *
+	 * @param string $content
+	 * @param array $attributes
+	 * @param Parser $parser
+	 * @return array
+	 */
+	static function html( $content, $attributes, $parser ) {
+		global $wgRawHtml, $wgRawHtmlNamespaces;
+
 		$title = $parser->getTitle();
 		if ( !isset( $title ) ) {
-			return true;
+			return htmlspecialchars( $content );
 		}
-
-		/**
-		 * Pass everything to core parser tag hook function for 'html'.
-		 * Enabled even when $wgRawHtml is disabled.
-		 *
-		 * This is potentially unsafe and should be used only in protected
-		 * namespaces, as the contents are emitted as raw HTML.
-		 */
 		$namespace = $title->getNamespace();
+
+		# Ideally, this check should take place in function 'addNamespaceHTML'
+		# but for some reason, $parser->getTitle() often returns null there.
 		# Note: This code prevents transcluding from a $wgRawHtmlNamespace
 		# to another namespace. Is that ideal?
 		if ( in_array( $namespace, $wgRawHtmlNamespaces ) ) {
 			$wgRawHtml = true;
-			$parser->setHook( 'html', array( 'CoreTagHooks', 'html' ) );
+			return CoreTagHooks::html( $content, $attributes, $parser );
 		}
 
-		return true;
+		# raw HTML not allowed here so just send out escaped text
+		return htmlspecialchars( Html::rawElement( 'html', $attributes, $content ) );
 	}
 
 }
